cc3e22b8e115bec085cdb13fc98e56332b5ad7a4
LeninHernandez <90071666+lenin123333@users.noreply.github.com>
2024-05-12 12:21:29 -0600

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './App.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <h1 className=' bg-slate-700 text-white'>Hola Mundo</h1>
+  </React.StrictMode>,
+)
