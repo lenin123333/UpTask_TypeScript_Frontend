f954092d3797a636167b84cfa0777c46433ed769
LeninHernandez <90071666+lenin123333@users.noreply.github.com>
2024-05-15 22:12:34 -0600

    nuevo formulario

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,6 +1,17 @@
 import { Link } from "react-router-dom"
+import {useForm} from 'react-hook-form'
 
 function CreateProjectView() {
+    const initialValues={
+        projectName:"",
+        clientName:"",
+        description:""
+    }
+    const {register,handleSubmit,formState:{errors}} = useForm({defaultValues:initialValues})
+
+    const handleForm =(data)=>{
+            console.log(data)
+    }
     return (
         <>
             <h1 className=" text-5xl font-black">Crear Proyecto</h1>
@@ -10,6 +21,13 @@ function CreateProjectView() {
                     Volver a Proyectos
                 </Link>
             </nav>
+
+            <form className=" mt-10 bg-white shadow-lg p-10 rounded-lg"
+                onSubmit={handleForm(handleForm)}
+                noValidate
+            >
+
+            </form>
 
         </>
     )
